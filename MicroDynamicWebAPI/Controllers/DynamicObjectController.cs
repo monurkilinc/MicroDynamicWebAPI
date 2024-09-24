1dbd74217c5c6e0948b87ded386fd44d053ca0c1
Onur Kilinc <108051080+monurkilinc@users.noreply.github.com>
2024-09-24 18:06:09 +0300

    Context.cs olusturuldu. DynamicObject.cs entitiesi olusturuldu. Migration islemi ile veritabani baglantisi gerceklestirildi. Controller olusturuldu ve CRUD metotlari eklendi.

---
+++ MicroDynamicWebAPI/Controllers/DynamicObjectController.cs
@@ -0,0 +1,77 @@
+﻿using MicroDynamicWebAPI.Datas;
+using MicroDynamicWebAPI.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Dynamic;
+using DynamicObject = MicroDynamicWebAPI.Entities.DynamicObject;
+
+namespace MicroDynamicWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DynamicObjectController : ControllerBase
+    {
+        //Dependency Injection
+        private readonly DataContext _context;
+
+        public DynamicObjectController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAllObjects()
+        {
+            var dynamicObjects = await _context.DynamicObjects.ToListAsync();
+
+            return Ok(dynamicObjects);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Entities.DynamicObject>> GetObjectById(int id)
+        {
+            var dynamicObject = await _context.DynamicObjects.FindAsync(id);
+            if(dynamicObject is null)
+            {
+                return NotFound("Dynamic Object not found.");
+            }
+
+            return Ok(dynamicObject);
+        }
+        [HttpPost]
+        public async Task<ActionResult<List<DynamicObject>>> AddObject(DynamicObject dynamicObject)
+        {
+            _context.DynamicObjects.Add(dynamicObject);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.DynamicObjects.ToListAsync());
+        }
+        [HttpPut]
+        public async Task<ActionResult<List<DynamicObject>>> UpdateObject(DynamicObject updateObject)
+        {
+            var dbObject = await _context.DynamicObjects.FindAsync(updateObject.Id);
+            if (dbObject is null)
+            {
+                return NotFound("Dynamic Object not found.");
+            }
+            dbObject.ObjectType = updateObject.ObjectType;
+            dbObject.Data = updateObject.Data;
+            dbObject.ParentId = updateObject.ParentId;
+
+            return Ok(await _context.DynamicObjects.ToListAsync());
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<List<DynamicObject>>> DeleteObject(int id)
+        {
+            var dbObject = await _context.DynamicObjects.FindAsync(id);
+            if (dbObject is null)
+            {
+                return NotFound("Dynamic Object not found.");
+            }
+            _context.DynamicObjects.Remove(dbObject);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.DynamicObjects.ToListAsync());
+        }
+    }
+}
---
+++ MicroDynamicWebAPI/Datas/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using MicroDynamicWebAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MicroDynamicWebAPI.Datas
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) :base(options)
+        {
+                
+        }
+        public DbSet<DynamicObject> DynamicObjects { get; set; }
+    }
+}
